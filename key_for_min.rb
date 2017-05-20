fe702ff9e7d9c5872abc9b905780544eabf11538
Jason Tseng <jason.f.tseng@gmail.com>
2017-05-20 05:27:10 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,24 @@
+require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+# collect all the values of the hash
+# sort the values with the lowest value first
+# take the first value of the array of sorted values and set it equal to a variable, minValue
+# iterate over each element of the hash and test if the value is equal to minValue
+# if it is, set the key equal to the variable minValueKey
+# return the variable minValueKey
+  minValues = Array.new
+  minValues << name_hash.collect  {|key, value| value}
+  minValues = minValues.flatten
+  minValues = minValues.sort
+  minValue = minValues[0]
+  minValueKey = nil
+  name_hash.each { |key, value|
+    if value == minValue
+      minValueKey = key
+    end
+  }
+  minValueKey
 end
